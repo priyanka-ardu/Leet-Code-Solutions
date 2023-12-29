88a5319334636d44310146877a62d9332c5ce6a1
Priyanka Ardu <106436962+hereforsomestuff@users.noreply.github.com>
2023-12-29 20:49:29 +0530

    Update two_sums.py

--- two_sums.py
+++ two_sums.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        if (2 <= len(nums) <= 10**4) and (-10**9 <= max(nums) <= 10**9) and (-10**9 <= target <= 10**9):
+            num_indices = {}
+            for i, num in enumerate(nums):
+                complement = target - num
+                if complement in num_indices:
+                    return [num_indices[complement], i]
+                num_indices[num] = i
+            return []    
+            
